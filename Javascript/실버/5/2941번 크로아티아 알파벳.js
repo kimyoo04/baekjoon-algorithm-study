e730228415d5da8bd2c696059ef7219c1287690d
kimyoo <58503130+kimyoo04@users.noreply.github.com>
2022-09-21 14:46:29 +0900

    [ Update ] - 2941번 크로아티아 알파벳
    - 파이썬에서는 replace를 이용해 아주 간단하게 구현이 가능하다.

--- Javascript/실버/5/2941번 크로아티아 알파벳.js
+++ Javascript/실버/5/2941번 크로아티아 알파벳.js
@@ -18,36 +18,36 @@ const input = fs
   .trim(); // c=c=
 
 // 모든 문자 각각 분할 및 할당
-const charList = input.split(""); // ["c", "=", "c", "="]
+const inputList = input.split(""); // ["c", "=", "c", "="]
 // 문자들의 개수 할당
-const charListLength = charList.length; // 4
+const inputListLength = inputList.length; // 4
 // 크로아티아 문자 할당
 const croatianAlphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
 // 크로아티아 문자 카운트할 변수 할당
-let count = 0;
+let wordCount = 0;
 
-// console.log(charList);
-// console.log(charListLength);
+// console.log(inputList);
+// console.log(inputListLength);
 
 // 반복문 돌려서 문자 2개씩 조합하기
-for (let index = 0; index < charListLength; index++) {
-  const combination = charList[index] + charList[index + 1]; // 문자 두 개씩 조합
+for (let index = 0; index < inputListLength; index++) {
+  const combination = inputList[index] + inputList[index + 1]; // 문자 두 개씩 조합 // c=
   // console.log(combination);
 
   // dz의 경우에만 뒤에 =이 오면 카운트
-  if (combination === "dz" && charList[index + 2] === "=") {
-    count++;
+  if (combination === "dz" && inputList[index + 2] === "=") {
+    wordCount++;
     index += 2;
 
     // 2개 조합 중에 크로아티아 알파벳이 있으면 카운트
   } else if (croatianAlphabet.includes(combination)) {
-    count++;
+    wordCount++;
     index += 1;
 
     // 그 외는 1글자 마다 카운트
   } else {
-    count++;
+    wordCount++;
   }
 }
 
-console.log(count);
+console.log(wordCount);
